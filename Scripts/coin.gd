12449cc30c86312933bf952d260a2c9c553762d6
Mick McCarthy <mick@mccarthy.tech>
2025-09-22 19:23:31 +0100

    Add collectable coins

---
+++ Scripts/coin.gd
@@ -0,0 +1,22 @@
+extends Area3D
+
+@export var rotation_speed: float = 180.0
+@export var bob_height: float = 0.2
+@export var bob_speed: float = 5.0
+
+@onready var start_y_pos: float = global_position.y
+
+func _process(delta: float) -> void:
+	rotation.y += deg_to_rad(rotation_speed) * delta
+	
+	var time = Time.get_unix_time_from_system()
+	var y_pos = (1 + sin(time * bob_speed) / 2) * bob_height + start_y_pos
+	
+	global_position.y = y_pos
+
+func _on_body_entered(body: Node3D) -> void:
+	if not body.is_in_group('Player'):
+		return
+	
+	# increase score
+	queue_free()
