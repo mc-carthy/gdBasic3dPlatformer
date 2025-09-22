98501ba7039b6af8cfd6b784f1540a3a80a0a164
Mick McCarthy <mick@mccarthy.tech>
2025-09-22 20:52:28 +0100

    Add menu screen

--- Scripts/player_controller.gd
+++ Scripts/player_controller.gd
@@ -36,7 +36,7 @@ func take_damage(amount: int) -> void:
 
 func game_over() -> void:
 	PlayerStats.score = 0
-	get_tree().reload_current_scene()
+	get_tree().change_scene_to_file('res://Scenes/menu.tscn')
 
 func increase_score(amount: int) -> void:
 	PlayerStats.score += amount
